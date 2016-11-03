ff6034b7e4c069bf3a466d4ac18e6e47222e166f
CMSInterkassa <cms@interkassa.com>
2016-11-03 23:23:50 +0200

    NetCat 5.7.0

--- netcat_ik/interkassa.php
+++ netcat_ik/interkassa.php
@@ -23,17 +23,12 @@ class nc_payment_system_interkassa extends nc_payment_system {
         // 'InvDesc' => null,
         );
 
-
-
     // получаемые параметры
     protected $callback_response = array(
 
         'PARAMETERS' => null
         );
 
-    /**
-     *
-     */
     public function execute_payment_request(nc_payment_invoice $invoice) {
         $amount = $invoice->get_amount("%0.2F");
         $currency = ($invoice->get_currency() == 'RUR') ? 'RUB' : $invoice->get_currency();
@@ -97,41 +92,26 @@ class nc_payment_system_interkassa extends nc_payment_system {
 
     public function validate_payment_callback_response(nc_payment_invoice $invoice = null) {
 
-     $this->wrlog('validate_payment_callback_response');
-
-     foreach ($_REQUEST as $key => $value) {
-         $str = $key.' => '.$value;
-         $this->wrlog($str);
-     }
-     $this->wrlog('--------');
-
-
-         if(count($_REQUEST) && $this->get_setting('IK_CO_ID') == $_REQUEST['ik_co_id']){
+         if(count($_POST)&& $this->checkIP() && $this->get_setting('IK_CO_ID') == $_POST['ik_co_id']){
 
             $this->wrlog('params ok');
 
-            if ($_REQUEST['ik_inv_st'] == 'success'){
+            if ($_POST['ik_inv_st'] == 'success'){
 
                 $this->wrlog('success');
 
-                if(isset($_REQUEST['ik_pw_via']) && $_REQUEST['ik_pw_via'] == 'test_interkassa_test_xts'){
+                if(isset($_POST['ik_pw_via']) && $_POST['ik_pw_via'] == 'test_interkassa_test_xts'){
                     $secret_key = $this->get_setting('TEST_KEY');
                 } else {
                     $secret_key = $this->get_setting('SECRET_KEY');
                 }
-                $this->wrlog($secret_key);
-
-
-
-                $request_sign = $_REQUEST['ik_sign'];
-
+                $request_sign = $_POST['ik_sign'];
                 $dataSet = [];
 
-                foreach ($_REQUEST as $key => $value) {
+                foreach ($_POST as $key => $value) {
                     if (!preg_match('/ik_/', $key)) continue;
                     $dataSet[$key] = $value;
                 }
-
                 unset($dataSet['ik_sign']);
                 ksort($dataSet, SORT_STRING); 
                 array_push($dataSet, $secret_key);  
@@ -141,16 +121,12 @@ class nc_payment_system_interkassa extends nc_payment_system {
                 if($request_sign != $sign){
                     $this->wrlog('Подписи не совпадают!');
                     die('Подписи не совпадают!');
-
                 }
             }
-
         }else{
-
             $this->wrlog('params didnt match');
             die('params didnt match');
         }
-
     }
 
     public function load_invoice_on_callback() {
@@ -160,11 +136,39 @@ class nc_payment_system_interkassa extends nc_payment_system {
         return $this->load_invoice($this->get_response_value('ik_pm_no'));
     }
 
-    public function wrlog($content){
+    //Функция для ведения лога
+    public function wrlog($content)
+    {
         $file = 'log.txt';
         $doc = fopen($file, 'a');
-        file_put_contents($file, PHP_EOL . $content, FILE_APPEND);  
+
+        file_put_contents($file, PHP_EOL . '====================' . date("H:i:s") . '=====================', FILE_APPEND);
+        if (is_array($content)) {
+            foreach ($content as $k => $v) {
+                if (is_array($v)) {
+                    wrlog($v);
+                } else {
+                    file_put_contents($file, PHP_EOL . $k . '=>' . $v, FILE_APPEND);
+                }
+            }
+        } else {
+            file_put_contents($file, PHP_EOL . $content, FILE_APPEND);
+        }
         fclose($doc);
+    }
+
+    public function checkIP()
+    {
+        $ip_stack = array(
+            'ip_begin' => '151.80.190.97',
+            'ip_end' => '151.80.190.104'
+        );
+
+        if (!ip2long($_SERVER['REMOTE_ADDR']) >= ip2long($ip_stack['ip_begin']) && !ip2long($_SERVER['REMOTE_ADDR']) <= ip2long($ip_stack['ip_end'])) {
+            wrlog('REQUEST IP' . $_SERVER['REMOTE_ADDR'] . 'doesnt match');
+            die('Ты мошенник! Пшел вон отсюда!');
+        }
+        return true;
     }
 }
 
